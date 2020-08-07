c81e69555a1dd7600e0d64433755985c1afa64d8
Felipe Damasceno <damassaa@gmail.com>
2020-08-07 01:48:35 -0300

    Front finished

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,33 +3,56 @@ import React from 'react'
 import WhatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../servces/api';
 
-function TeacherItem() {
+export interface Teacher {
+    avatar: string;
+    bio: string;
+    cost: number;
+    id: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('/connections', {
+            user_id: teacher.id,
+        });
+    }
+
     return(
         <article className="teacher-item">
             <header>
-                <img src="https://avatars3.githubusercontent.com/u/2249891?s=460&u=38bf09b62f61ece8b93865e4e5faaa3a2c8bca01&v=4" alt="Felipe Leal Damasceno"/>
+                <img src={teacher.avatar} alt={teacher.name}/>
                 <div>
-                    <strong>Felipe Leal Damasceno</strong>
-                    <span>História</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
             <p>
-                Entusiasta dos melhores livros de história que existem.
-                <br/><br/>
-                Apaixonado por surrar bolsocucks intelectualmente e por mudar a vida das pessoas através de experiências e fatos.
+                {teacher.bio}
             </p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 80,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a 
+                    target="_blank" 
+                    onClick={createNewConnection} 
+                    href={`https://wa.me/${teacher.whatsapp}`}
+                >
                     <img src={WhatsappIcon} alt="WhatsApp"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     );
